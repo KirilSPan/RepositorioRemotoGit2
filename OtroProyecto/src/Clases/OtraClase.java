350a66d7820541574d4c1ff5dfc82971e7ea7bf9
Kirilsp <Kirilsp@DESKTOP-APD0L4R.localdomain>
2023-04-19 20:11:33 +0200

    Commit con Atributos en segunda clase

--- OtroProyecto/src/Clases/OtraClase.java
+++ OtroProyecto/src/Clases/OtraClase.java
@@ -3,4 +3,17 @@ package Clases;
 public class OtraClase {
 
 	int otroAtributo;
+
+	@Override
+	public String toString() {
+		return "OtraClase [otroAtributo=" + otroAtributo + "]";
+	}
+
+	public int getOtroAtributo() {
+		return otroAtributo;
+	}
+
+	public void setOtroAtributo(int otroAtributo) {
+		this.otroAtributo = otroAtributo;
+	}
 }
