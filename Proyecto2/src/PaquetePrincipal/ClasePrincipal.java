3f3277c0d1868f6b47816c2154f3881557a5b137
Kirilsp <Kirilsp@Equipo10Kiril.dam2.local>
2023-05-29 23:28:30 +0200

    Commit rama1

---
+++ Proyecto2/src/PaquetePrincipal/ClasePrincipal.java
@@ -0,0 +1,16 @@
+package PaquetePrincipal;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mensaje Clase Principal");
+		
+		System.out.println("Mensaje 1 Clase Principal Rama1");
+		System.out.println("Mensaje 2 Clase Principal Rama1");
+
+
+	}
+
+}
